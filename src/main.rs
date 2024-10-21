0681af1633fc531cfec29360f4249b5ee162a2c7
G2-Games <ke0bhogsg@gmail.com>
2024-10-21 13:48:06 -0500

    Added blake3 hashing of uploaded files

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use std::path::PathBuf;
-
+use std::path::{Path, PathBuf};
+use blake3::Hash;
 use maud::{html, Markup, DOCTYPE, PreEscaped};
-
-use rand::Rng;
 use rocket::{
-    form::Form, fs::{FileServer, Options, TempFile}, get, post, routes, FromForm
+    form::Form, fs::{FileServer, Options, TempFile}, get, post, routes, tokio::{fs::File, io::AsyncReadExt}, FromForm
 };
+use uuid::Uuid;
 
 fn head(page_title: &str) -> Markup {
     html! {
         (DOCTYPE)
-        meta charset="utf-8";
+        meta charset="UTF-8";
         title { (page_title) }
         // Javascript stuff for client side handling
         script { (PreEscaped(include_str!("static/form_handler.js"))) }
@@ -24,19 +23,31 @@ fn home() -> Markup {
     html! {
         (head("Mochi"))
         body {
-            h1 { "File Hosting!" }
-            form id="uploadForm" {
-                label for="fileUpload" class="file-upload" onclick="document.getElementById('fileInput').click()" {
-                    "Upload File"
+            div class="main-wrapper" {
+                form id="uploadForm" {
+                    label for="fileUpload" class="file-upload" onclick="document.getElementById('fileInput').click()" {
+                        "Upload File"
+                    }
+                    input id="fileInput" type="file" name="fileUpload" onchange="formSubmit(this.parentNode)" style="display:none;";
                 }
-                input id="fileInput" type="file" name="fileUpload" onchange="formSubmit(this.parentNode)" style="display:none;";
-            }
-            div class="progress-box" {
-                progress id="uploadProgress" value="0" max="100" {}
-                p id="uploadProgressValue" class="progress-value" { "0%" }
+                div class="progress-box" {
+                    progress id="uploadProgress" value="0" max="100" {}
+                    p id="uploadProgressValue" class="progress-value" { "0%" }
+                }
             }
         }
     }
+}
+
+struct Database {
+    files: Vec<File>
+}
+
+struct MochiFile {
+    /// The original name of the file
+    name: String,
+    /// The hashed contents of the file as a Blake3 hash
+    hash: Hash,
 }
 
 #[derive(FromForm)]
@@ -49,21 +60,42 @@ struct Upload<'r> {
 #[post("/upload", data = "<file_data>")]
 async fn handle_upload(mut file_data: Form<Upload<'_>>) -> Result<(), std::io::Error> {
     let mut out_path = PathBuf::from("files/");
-    out_path.push(get_filename(file_data.file.name().unwrap()));
-    file_data.file.persist_to(out_path).await?;
+
+    // Get temp path and hash it
+    let temp_filename = "temp_files/".to_owned() + &Uuid::new_v4().to_string();
+    file_data.file.persist_to(&temp_filename).await?;
+    let hash = hash_file(&temp_filename).await?;
+
+    out_path.push(get_filename(
+        // TODO: Properly sanitize this...
+        file_data.file.raw_name().unwrap().dangerous_unsafe_unsanitized_raw().as_str(),
+        hash
+    ));
+
+    // Move it to the new proper place
+    std::fs::rename(temp_filename, out_path)?;
 
     Ok(())
 }
 
-/// Get a random filename for use as the uploaded file's name
-fn get_filename(name: &str) -> String {
-    let rand_string: String = rand::thread_rng()
-        .sample_iter(&rand::distributions::Alphanumeric)
-        .take(8)
-        .map(char::from)
-        .collect();
+async fn hash_file<P: AsRef<Path>>(input: &P) -> Result<Hash, std::io::Error> {
+    let mut file = File::open(input).await?;
+    let mut buf = vec![0; 5000000];
+    let mut hasher = blake3::Hasher::new();
 
-    let uuid = rand_string + "_" + name;
+    let mut bytes_read = None;
+    while bytes_read != Some(0) {
+        bytes_read = Some(file.read(&mut buf).await?);
+        hasher.update(&buf[..bytes_read.unwrap()]);
+        dbg!(bytes_read);
+    }
+
+    Ok(hasher.finalize())
+}
+
+/// Get a random filename for use as the uploaded file's name
+fn get_filename(name: &str, hash: Hash) -> String {
+    let uuid = hash.to_hex()[0..10].to_string() + "_" + name;
     uuid
 }
 
