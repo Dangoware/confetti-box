8bb4e5fac4e06005903b76edbd0ed3dd031dbff2
G2-Games <ke0bhogsg@gmail.com>
2024-10-21 05:05:19 -0500

    Improved everything, still only uploads

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,20 @@ use std::path::PathBuf;
 
 use maud::{html, Markup, DOCTYPE, PreEscaped};
 
+use rand::Rng;
 use rocket::{
     form::Form, fs::{FileServer, Options, TempFile}, get, post, routes, FromForm
 };
-use uuid::Uuid;
-
-const FORM_HANDLER_JS: &str = include_str!("js/form_handler.js");
 
 fn head(page_title: &str) -> Markup {
     html! {
         (DOCTYPE)
         meta charset="utf-8";
         title { (page_title) }
-        script { (PreEscaped(FORM_HANDLER_JS)) }
+        // Javascript stuff for client side handling
+        script { (PreEscaped(include_str!("static/form_handler.js"))) }
+        // CSS for styling the sheets
+        style { (PreEscaped(include_str!("static/main.css"))) }
     }
 }
 
@@ -24,11 +25,15 @@ fn home() -> Markup {
         (head("Mochi"))
         body {
             h1 { "File Hosting!" }
-            h2 { "Everything will be deleted in like 24 hours or something idk" }
             form id="uploadForm" {
-                input type="file" name="fileUpload" onchange="formSubmit(this.parentNode)" {
-                    "Select File (Or drag here)"
+                label for="fileUpload" class="file-upload" onclick="document.getElementById('fileInput').click()" {
+                    "Upload File"
                 }
+                input id="fileInput" type="file" name="fileUpload" onchange="formSubmit(this.parentNode)" style="display:none;";
+            }
+            div class="progress-box" {
+                progress id="uploadProgress" value="0" max="100" {}
+                p id="uploadProgressValue" class="progress-value" { "0%" }
             }
         }
     }
@@ -44,15 +49,21 @@ struct Upload<'r> {
 #[post("/upload", data = "<file_data>")]
 async fn handle_upload(mut file_data: Form<Upload<'_>>) -> Result<(), std::io::Error> {
     let mut out_path = PathBuf::from("files/");
-    out_path.push(get_filename());
+    out_path.push(get_filename(file_data.file.name().unwrap()));
     file_data.file.persist_to(out_path).await?;
 
     Ok(())
 }
 
 /// Get a random filename for use as the uploaded file's name
-fn get_filename() -> String {
-    let uuid = Uuid::new_v4().to_string();
+fn get_filename(name: &str) -> String {
+    let rand_string: String = rand::thread_rng()
+        .sample_iter(&rand::distributions::Alphanumeric)
+        .take(8)
+        .map(char::from)
+        .collect();
+
+    let uuid = rand_string + "_" + name;
     uuid
 }
 
